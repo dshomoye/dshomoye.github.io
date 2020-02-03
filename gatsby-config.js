25c3bb16008052e298900ca13b595af7c5aa303e
Adedamola Shomoye <dshomoye@gmail.com>
2020-02-03 12:12:02 -0500

    better state

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              isIconAfterHeader: true
+            }
+          },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
