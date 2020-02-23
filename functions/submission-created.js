83158124641ff14d544c4593c1fd0befca275e80
Adedamola Shomoye <dshomoye@gmail.com>
2020-02-22 22:33:25 -0500

    and it's done

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,9 +1,24 @@
-exports.handler = function(event, context, callback) {
-  // your server-side functionality
-  console.log('event', event)
-  console.log('ctx', context)
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+const got = require("got")
+
+exports.handler = async function(event) {
+  const eventData = JSON.parse(event.body)
+  try {
+    const response = await got("https://api.sendinblue.com/v3/contacts", {
+      json: { email: eventData.email },
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "api-key": process.env.SENDINBLUE_API_KEY,
+      },
+    })
+    return {
+      statusCode: response.statusCode,
+      body: "success",
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: event.body,
+    }
+  }
 }
