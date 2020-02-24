3bf94cfd785b00df5448fe0eea87feaa30fab622
Adedamola Shomoye <dshomoye@gmail.com>
2020-02-24 18:25:48 -0500

    alright cool

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {		
+      resolve: `gatsby-plugin-scroll-reveal`,		
+      options: {		
+        threshold: 0.05,		
+        rootMargin: "100px 0px",		
+      },		
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
