5dfdc4c38b27cd09eb68cb0352ea9536ab9bcc18
Damola <dshomoye@gmail.com>
2020-08-02 11:29:12 -0700

    basic

---
+++ src/hooks/useResize.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react'
+
+const useResize = (ref) => {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log('resizing for ', ref.current)
+      setWidth(ref.current.offsetWidth)
+      setHeight(ref.current.offsetHeight)
+    }
+
+    if(ref) handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [ref])
+
+  return { width, height }
+}
+
+export default useResize
--- src/pages/privacy-report-card.js
+++ src/pages/privacy-report-card.js
@@ -2,12 +2,53 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import Table from "../components/Table"
+
+const privcayData = [
+  {
+    id: 1,
+    name: "Google",
+    crossSiteTracking: 5,
+    locationTracking: 5,
+    dataRetention: 4,
+    openness: 4,
+    usercontrol: 4,
+    darkPatterns: 5,
+  },
+]
+
+const dataColumns = [
+  {
+    key: "name",
+    title: "Site Name",
+    width: 100
+  },
+  {
+    key: "crossSiteTracking",
+    title: "Cross Site Tracking",
+    width: 100
+  },
+  {
+    key: "locationTracking",
+    title: "Location Tracking",
+    width: 100
+  },
+  {
+    key: "dataRetention",
+    title: "Data Retentation",
+    width: 100
+  },
+]
 
 const PrivacyReportCard = ({ data, location }) => {
-  const {title} = data.site.siteMetadata
+  const { title } = data.site.siteMetadata
   return (
     <Layout title={title} location={location} fullWidth>
-      <h2>Privacy Report: Summary of privacy policies of (popular) services online .</h2>
+      <h2>
+        Privacy Report: Summary of privacy policies of (popular) services.
+      </h2>
+      <hr />
+      <Table columns={dataColumns} data={privcayData} rowKey="id" />
     </Layout>
   )
 }
