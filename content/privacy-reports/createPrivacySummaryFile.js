c0e102d5f9ae9949902cad6cfe10cef23c74d367
Damola <dshomoye@gmail.com>
2020-08-29 02:17:50 -0700

    debug

--- content/privacy-reports/createPrivacySummaryFile.js
+++ content/privacy-reports/createPrivacySummaryFile.js
@@ -5,14 +5,12 @@ const content = process.env.FILE_CONTENT
 
 const siteNameRegex = /\[TO-PR\] \[(\w+)\]/
 
-console.log(issueTitle)
-console.log(content)
-
 const match = issueTitle.match(siteNameRegex)
 const siteName = match && match.length > 1 ? match[1] : null
 if(content && siteName) {
   console.log(`Creating file with content ${siteName}.md`)
-  fs.writeFileSync(`${siteName}.md`, content)
-  fs.readFileSync(`${siteName}.md`)
+  fs.writeFileSync(`content/privacy-reports/${siteName}.md`, content)
+  console.log('File Content:')
+  console.log(fs.readFileSync(`content/privacy-reports/${siteName}.md`))
   console.log('DONE')
 }
