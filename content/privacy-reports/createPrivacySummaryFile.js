6d4cc6aefdf602c1a3fd2cc5aeddb0c2d0a65a94
Damola <dshomoye@gmail.com>
2020-08-29 02:38:18 -0700

    test variable branch

--- content/privacy-reports/createPrivacySummaryFile.js
+++ content/privacy-reports/createPrivacySummaryFile.js
@@ -1,16 +1,12 @@
 const fs = require("fs")
-
 const issueTitle = process.env.ISSUE_TITLE
 const content = process.env.FILE_CONTENT
-
 const siteNameRegex = /\[TO-PR\] \[(\w+)\]/
-
 const match = issueTitle.match(siteNameRegex)
 const siteName = match && match.length > 1 ? match[1] : null
+
 if(content && siteName) {
   console.log(`Creating file with content ${siteName}.md`)
   fs.writeFileSync(`content/privacy-reports/${siteName}.md`, content)
-  console.log('File Content:')
-  console.log(fs.readFileSync(`content/privacy-reports/${siteName}.md`, "utf-8"))
   console.log('DONE')
 }
