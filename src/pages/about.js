611512220bf5d2cc945dfff8aca9e75fb37ebd64
Damola <dshomoye@gmail.com>
2021-01-13 22:29:08 -0800

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
 
 import Layout from "../components/layout"
@@ -44,28 +44,21 @@ class NotFoundPage extends React.Component {
             <h1>About Me</h1>
             <section>
               <p>
-                I’m a full stack developer at Splunk; Before that, I was a software engineer at Capital One (they are totally a tech company!).
-              </p>
-              <p>
+                I’m a Full-Stack Software Engineer at Splunk; Previously, I was a software engineer at Capital One (they are totally a tech company!).
+                <br />
+                I primarily write in JavaScript and Python, 
+                but I've worked in Go and Java too, and always trying out new languages, like <Link to="/trying-rust-lang">Rust</Link>.
+                <br />
+                <br />
                 Believe this, I love everything tech. In a “the future is gonna
                 be so awesome” kind of way. All cautiously of course. We have to
                 stay on sky net’s good side.
-              </p>
-
-              <p>
-                {`I have a PlayStation 4 pro and it is how I usually unwind at the end of day. 
-                I really don't have a lot of options anyway 🥺. #2020`}
-              </p>
-              <p>
-                {`I like to hike. This is a new...err... "hobby" for me, while I was living in Virginia. 
-                It’s nice to be... *one with nature*. I'm now based out of the bay area. `}
-              </p>
-              <p>
-                {`I like to travel. I don’t have a long list of mind-blowing places
-                I’ve been to, yet, but trust me. I like to travel.`}
-              </p>
-
-              <p>
+                <br />
+                I'm always looking for something new to learn or work on. 
+                And I occasionally travel, which I hope to return to when regular civilization is restored.
+                <br />
+                Video games are a fun pass time too.
+                <br />
                 I grew up in Nigeria. It’s not Wakanda, but it was great in a very special way, I loved it.
               </p>
             </section>
