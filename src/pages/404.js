bfd52d25b411a1d4a03d084ebb0c0d0ceb057da6
Damola Shomoye <dshomoye@users.noreply.github.com>
2021-05-01 09:22:05 -0700

    no lotties files, add new post on elm (#65)
    * no lotties files, add new post
    
    * Update ghpages.yml
    
    * Update ghpages.yml
    
    * Update ghpages.yml
    
    * Update ghpages.yml
    
    * update submodule path
    
    * update pages branch

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,34 +1,17 @@
-import React, { Suspense, lazy } from "react"
+import React from "react"
 import { graphql, Link } from "gatsby"
-const Lottie = lazy(() => import("react-lottie"))
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import animationData from "../lotties/404.json"
 
 const NotFoundPage = (props) => {
   const { data } = props
   const siteTitle = data.site.siteMetadata.title
-  const isSSR = typeof window === "undefined"
 
   return (
     <Layout location={props.location} title={siteTitle}>
       <SEO title="404: Not Found" />
       <Link to="/">
-        {!isSSR && (
-          <Suspense fallback={<h3>Page was not found...</h3>}>
-            <Lottie
-              options={{
-                loop: true,
-                autoplay: true,
-                animationData,
-                rendererSettings: {
-                  preserveAspectRatio: "xMidYMid slice",
-                },
-              }}
-            />
-          </Suspense>
-        )}
         <p>This page is not real, you imagined it.</p>
         <p>Go Home.</p>
       </Link>
