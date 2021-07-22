00dc75940f89d201f05f6ccfbfa9963d43f3e1a4
Damola Shomoye <dshomoye@users.noreply.github.com>
2021-07-22 07:11:33 -0700

    update about page (#66)
    * update about
    
    * ssh submodule

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
+class AboutPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
@@ -42,23 +42,22 @@ class NotFoundPage extends React.Component {
             <h1>About Me</h1>
             <section>
               <p>
-                I’m a Full-Stack Software Engineer at Splunk; Previously, I was
-                a software engineer at Capital One (they are totally a tech
+                I’m a software engineer at Splunk; before that, I was
+                at Capital One (they are totally a tech
                 company!).
-                <br />I primarily write in JavaScript and Python, but I have
-                worked in Go and Java too, and always trying out new languages,
+                <br />I've worked with JavaScript and Python, I mainly write Go now and always trying out new languages,
                 like <Link to="/trying-rust-lang">Rust</Link>.
                 <br />
                 <br />
-                Believe this, I love everything tech. In a “the future is gonna
+                Believe this, I love everything tech. In a "future is gonna
                 be so awesome” kind of way. All cautiously of course. We have to
                 stay on sky net’s good side.
                 <br />
                 I am always looking for something new to learn or work on. And I
-                occasionally travel, which I hope to return to when regular
-                civilization is restored.
+                occasionally travel, which I am trying to get back to now, since 2020 happened.
                 <br />
-                Video games are a fun pass time too.
+                Video games are a fun pass time too. 
+                <br />I particularly recommend (non-exhaustive): Legend of Zelda: Breath of the Wild; Ghost of Tsushima and Control, in that order.
                 <br />I grew up in Nigeria. It’s not Wakanda, but it was great
                 in a very special way, I loved it.
               </p>
@@ -70,7 +69,7 @@ class NotFoundPage extends React.Component {
   }
 }
 
-export default NotFoundPage
+export default AboutPage
 
 export const pageQuery = graphql`
   {
