44b4d8d1e561479e79c3df7f51f0110553fb9f2f
Damola <dshomoye@gmail.com>
2022-06-25 09:35:28 -0700

    fix typos

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
+class AboutPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
@@ -42,25 +42,23 @@ class NotFoundPage extends React.Component {
             <h1>About Me</h1>
             <section>
               <p>
-                I’m a Full-Stack Software Engineer at Splunk; Previously, I was
-                a software engineer at Capital One (they are totally a tech
-                company!).
+                I’m a Software Engineer at Gem; 
+                99.9% sure I am not a bot, but I get paid to talk to computers so who knows.
                 <br />I primarily write in JavaScript and Python, but I have
-                worked in Go and Java too, and always trying out new languages,
+                worked in Go too, and always trying out new languages,
                 like <Link to="/trying-rust-lang">Rust</Link>.
                 <br />
                 <br />
                 Believe this, I love everything tech. In a “the future is gonna
-                be so awesome” kind of way. All cautiously of course. We have to
+                be so awesome” kind of way. All cautiously of course. Have to
                 stay on sky net’s good side.
                 <br />
                 I am always looking for something new to learn or work on. And I
-                occasionally travel, which I hope to return to when regular
-                civilization is restored.
+                occasionally travel.
                 <br />
                 Video games are a fun pass time too.
-                <br />I grew up in Nigeria. It’s not Wakanda, but it was great
-                in a very special way, I loved it.
+                <br />I grew up in Nigeria. It’s no Wakanda, but it was great
+                in a very special way.
               </p>
             </section>
           </article>
@@ -70,7 +68,7 @@ class NotFoundPage extends React.Component {
   }
 }
 
-export default NotFoundPage
+export default AboutPage
 
 export const pageQuery = graphql`
   {
